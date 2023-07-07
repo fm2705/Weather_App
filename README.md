5db779e5472a9acfde4483c169144d10174477ee
Ludmilla <ludmillajani@yahoo.com>
2023-07-06 23:02:32 -0400

    add files all

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Weather_App
 Weather App using Python-Flask
+
+source venv/bin/activate
+source venv/Scripts/activate
+pip install -r requirements.txt
+(Successfully installed Flask-2.3.2 Jinja2-3.1.2 MarkupSafe-2.1.3 Werkzeug-2.3.6 blinker-1.6.2 certifi-2023.5.7 charset-normalizer-3.1.0 click-8.1.3 colorama-0.4.6 idna-3.4 itsdangerous-2.1.2 python-dotenv-1.0.0 requests-2.31.0 urllib3-2.0.)
+https://home.openweathermap.org/
+
+index.html: ! + ENTER
---
+++ weather.py
@@ -0,0 +1,48 @@
+import requests
+from dotenv import load_dotenv
+import os
+from dataclasses import dataclass
+
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+#print(api_key)
+
+@dataclass
+class WeatherData:
+    main: str
+    description: str
+    icon: str
+    temperature: int
+
+def get_lan_lon(city_name, state_name, country_name, API_key):
+    resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_name},{country_name}&appid={API_key}').json()
+    data = resp[0]
+    lat, lon = data.get('lat'), data.get('lon')
+    #print(resp)
+    return lat, lon
+
+print(get_lan_lon('Toronto', 'ON', 'Canada', api_key))
+
+def get_current_weather(lat, lon, API_key):
+    resp = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}&units=metric').json()
+    data = WeatherData(
+        main = resp.get('weather')[0].get('main'),
+        description= resp.get('weather')[0].get('description'),
+        icon = resp.get('weather')[0].get('icon'),
+        temperature=int(resp.get('main').get('temp'))
+    )
+    #print(data)
+    return data
+
+def main(city_name, state_name, country_name):
+    lat, lon = get_lan_lon(city_name, state_name, country_name, api_key)
+    weather_data = get_current_weather(lat, lon, api_key)
+    print(weather_data)
+    return weather_data
+
+if __name__ == "__main__":
+    lat, lon = get_lan_lon('Toronto', 'ON', 'Canada', api_key)
+    (get_current_weather(lat, lon, api_key))
+    #print(get_current_weather(lat, lon, api_key))
+
